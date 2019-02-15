ac3758dd47623b8bf8aac390558712e505c70043
Aidan Smithers <porrige51122>
2019-02-15 22:48:18 +0000

    Updated vector class

--- assets/js/components/vector.js
+++ assets/js/components/vector.js
@@ -4,9 +4,67 @@ class Vector {
     this.y = y;
   }
 
+  str() {
+    return "(" + this.x + "," + this.y + ")";
+  }
+
+  eq(other) {
+    return this.x == other.x && this.y == other.y;
+  }
+
+  ne(other) {
+    return !this.eq(other);
+  }
+
+  getP() {
+    return [this.x, this.y];
+  }
+
+  copy() {
+    let v = new Vector(this.x, this.y);
+    return v;
+  }
+
   add(other) {
     this.x += other.x;
     this.y += other.y;
+    return this;
+  }
+
+  subtract(other) {
+    return this.add(-other);
+  }
+
+  dot(other) {
+    return this.x * other.x + this.x * other.x;
+  }
+
+  multiply(k) {
+    this.x *= k;
+    this.y *= k;
+    return this;
+  }
+
+  negate() {
+    return this.multiply(-1);
+  }
+
+  divide(k) {
+    this.multiply(1/k);
+  }
+
+  normalize() {
+    return this.divide(this.length());
+  }
+
+  length() {
+    return Math.sqrt(this.x**2 + this.y**2);
+  }
+
+  reflect(normal) {
+    n = normal.copy();
+    n.multiply( 2 * this.dot(normal) );
+    this.subtract(n);
     return this;
   }
 }
